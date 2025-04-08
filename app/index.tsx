05d7e8b6bed866ce4a378b6b43d8366ef50d9763
dkmqflx <dkmqflx@gmail.com>
2025-04-08 13:20:39 +0900

    8 - View

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,23 @@
 import { Text, View } from "react-native";
 
+/**
+ * View is a fundamental component in React Native:
+ * - It's the equivalent of a <div> in web development
+ * - Acts as a container for other components
+ * - Used for creating layouts and structuring content
+ * - Supports flexbox layout system
+ * - Can be nested inside other Views
+ * - Handles touch events and can be styled
+ */
 export default function Index() {
   return (
     <View
       style={{
+        // flex: 1 makes the View take up all available space
         flex: 1,
+        // Centers content vertically in the container
         justifyContent: "center",
+        // Centers content horizontally in the container
         alignItems: "center",
       }}
     >
