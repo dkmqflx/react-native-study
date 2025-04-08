3c2097186b8dbd2f97082c4f2e102e58dd189f63
dkmqflx <dkmqflx@gmail.com>
2025-04-08 23:06:43 +0900

    27 - Flex

--- app/index.tsx
+++ app/index.tsx
@@ -4,8 +4,8 @@ import Box from "../components/box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
-      <Box style={{ backgroundColor: "#b65d1f" }}>Box 2</Box>
+      <Box style={{ backgroundColor: "#8e9b00", flex: 1 }}>Box 1</Box>
+      <Box style={{ backgroundColor: "#b65d1f", flex: 3 }}>Box 2</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
       <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
       <Box style={{ backgroundColor: "#6b0803" }}>Box 5</Box>
@@ -17,6 +17,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    // because of flex: 1, the container will take the full height of the screen
     flex: 1,
     marginTop: 64,
     borderWidth: 6,
