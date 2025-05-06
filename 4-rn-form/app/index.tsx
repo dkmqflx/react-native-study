a5807dbdb2b875dc5284deb9b318c7fc4fda1e13
dkmqflx <dkmqflx@gmail.com>
2025-05-07 02:45:12 +0900

    58 - TextInput

--- 4-rn-form/app/index.tsx
+++ 4-rn-form/app/index.tsx
@@ -1,10 +1,41 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+  TextInput,
+} from "react-native";
 
 export default function App() {
+  const [username, setUsername] = useState("");
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your name"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <Text>My name is {username}</Text>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: StatusBar.currentHeight,
+  },
+  input: {
+    height: 40,
+    borderColor: "#ddd",
+    borderWidth: 1,
+    marginBottom: 15,
+    padding: 10,
+    borderRadius: 5,
+  },
+});
