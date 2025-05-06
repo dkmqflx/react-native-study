21930551c3189330e2a06481692608fcf72bef79
dkmqflx <dkmqflx@gmail.com>
2025-05-06 23:07:31 +0900

    51 - Lists

--- 3-rn-list/app/index.tsx
+++ 3-rn-list/app/index.tsx
@@ -1,9 +1,52 @@
-import { View, Text } from "react-native";
+import {
+  StyleSheet,
+  View,
+  ScrollView,
+  Text,
+  StatusBar,
+  SafeAreaView,
+} from "react-native";
+import pokemonList from "../data.json";
 
 export default function App() {
+  /**
+   * SafeAreaView is for iOS status bar (For example, iPhone X notch)
+   * scrollView is for horizontal scrolling
+   */
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        {pokemonList.map((pokemon) => {
+          return (
+            <View style={styles.card} key={pokemon.id}>
+              <Text style={styles.cardText}>{pokemon.type}</Text>
+              <Text style={styles.cardText}>{pokemon.name}</Text>
+            </View>
+          );
+        })}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5F5F5",
+    // Add padding at the top equal to the status bar height (Android only)
+    paddingTop: StatusBar.currentHeight,
+  },
+  scrollView: {
+    paddingHorizontal: 16,
+  },
+  card: {
+    backgroundColor: "#FFFFFF",
+    padding: 16,
+    borderRadius: 8,
+    marginBottom: 16,
+    borderWidth: 1,
+  },
+  cardText: {
+    fontSize: 30,
+  },
+});
